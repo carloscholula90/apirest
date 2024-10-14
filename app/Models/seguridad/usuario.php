7d0aed277e8bb986e9cfce358b237b9e4c02d694
hdz.pimentel.isabel <hdz.pimentel.isabel>
2024-10-13 22:26:21 -0600

    IPH 13/10/2024: Reestablecer modelo de usuario

--- app/Models/seguridad/usuario.php
+++ app/Models/seguridad/usuario.php
@@ -1,10 +1,20 @@
 <?php
-namespace App\Models\seguridad;
+
+namespace App\Models\general;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class usuario extends Model
-{
+class Usuario extends Model{
+
     use HasFactory;
+    protected $table = 'usuario';
+    public $incrementing = false;
+    protected $primaryKey = ['uid','secuencial'];
+    public $timestamps = false;
+
+    public function persona(){
+        return $this->belongsTo(Persona::class);
+    }
+    
 }
