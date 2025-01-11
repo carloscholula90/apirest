9c83b5aa8ccdf667b79cac911693f22af21e9b80
HdzPimentelIsabel <hdz.pimentel.isabel@gmail.com>
2025-01-11 00:24:16 -0600

    IPH 11/01/2025: Impresión de reporte

--- app/Http/Controllers/Api/seguridad/ModuloController.php
+++ app/Http/Controllers/Api/seguridad/ModuloController.php
@@ -127,7 +127,7 @@ class ModuloController extends Controller
         })->toArray();   
     
         return $this->pdfController->generateReport($modulosArray,$columnWidths,$keys , 'REPORTE DE MÓDULOS', $headers,'L','letter',
-        'rptModulos'.mt_rand(1, 100).'pdf');
+        'rptModulos'.mt_rand(1, 100).'.pdf');
       
     }  
 }
