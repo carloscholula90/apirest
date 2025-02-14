3e6db03ff90f161f153c4f0adb55d3119c37013b
HdzPimentelIsabel <hdz.pimentel.isabel@gmail.com>
2025-02-13 20:49:11 -0600

    IPH 13/02/2025

--- routes/general/personas.php
+++ routes/general/personas.php
@@ -3,12 +3,13 @@ use App\Http\Controllers\Api\general\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route; 
 
+
+Route::post('/generaReporte', [PersonaController::class, 'generaReportePersonas']);
 Route::get('/buscar/{var}', [PersonaController::class, 'getPersonasLike']);
 Route::get('/contacto/{id}', [PersonaController::class, 'recovery']);
 Route::get('/{id}', [PersonaController::class, 'show']);
 Route::get('/', [PersonaController::class, 'index']);
 Route::post('/create', [PersonaController::class, 'store']);
-Route::post('/generaReporte', [PersonaController::class, 'generaReportePersonas']);
 Route::put('/{id}', [PersonaController::class, 'update']);
 Route::delete('/{id}', [PersonaController::class, 'destroy']);   
   
