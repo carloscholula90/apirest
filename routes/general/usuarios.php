72b86b01b72fe5884b140a0da4ca96de77f5d081
HdzPimentelIsabel <hdz.pimentel.isabel@gmail.com>
2025-04-21 21:45:08 -0600

    IPH 21/04/2025: Agregar usuarios

--- routes/general/usuarios.php
+++ routes/general/usuarios.php
@@ -3,7 +3,7 @@ use App\Http\Controllers\Api\seguridad\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route; 
    
-//Route::get('/', [UsuarioController::class, 'index']);
+Route::get('/', [UsuarioController::class, 'index']);
 Route::get('/{id}/{pasw}', [UsuarioController::class, 'show']);  
 Route::post('/create', [UsuarioController::class, 'store']);
 Route::put('/', [UsuarioController::class, 'update']);          
