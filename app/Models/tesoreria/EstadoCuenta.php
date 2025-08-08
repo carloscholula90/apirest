12cc714e4e755e1061149a68e1191ace6155e5ed
HdzPimentelIsabel <hdz.pimentel.isabel@gmail.com>
2025-08-07 20:21:46 -0600

    IPH 07/08/2025: agregar uidcajero

--- app/Models/tesoreria/EstadoCuenta.php
+++ app/Models/tesoreria/EstadoCuenta.php
@@ -11,6 +11,6 @@ class EstadoCuenta extends Model
     protected $table = 'edocta';
     public $incrementing = false;
     protected $fillable = [ 'uid','secuencia','idServicio','consecutivo','importe','idPeriodo','fechaMovto','referencia',
-                            'idformaPago','cuatrodigitos','tipomovto','FechaPago','folio'];
+                            'idformaPago','cuatrodigitos','tipomovto','FechaPago','folio','idcajero'];
     public $timestamps = false;
 }
